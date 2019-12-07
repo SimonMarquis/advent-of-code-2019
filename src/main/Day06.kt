3926c93800c870f94270e5e49164a8b8891340a7
Simon Marquis <contact@simon-marquis.fr>
2019-12-07 13:03:02 +0100

    Day 6 - Move Graph class into Day06

--- src/main/Day06.kt
+++ src/main/Day06.kt
@@ -10,57 +10,56 @@ class Day06(input: List<String>) {
 
     fun part2(src: String, dst: String) = graph.numberOfOrbitalTransfers(src, dst)
 
-}
+    private class Graph(input: List<String>) {
 
-private class Graph(input: List<String>) {
-
-    companion object {
-        private val pattern = """(\w+)\)(\w+)""".toRegex()
-    }
-
-    val orbitedBy: Map<String, Set<String>>
-    val adjacent: Map<String, Set<String>>
-
-    init {
-        val orbitedBy = mutableMapOf<String, MutableSet<String>>()
-        val adjacent = mutableMapOf<String, MutableSet<String>>()
-        input.forEach {
-            val (parent, satellite) = pattern.find(it)!!.destructured
-            orbitedBy.getOrPut(parent) { mutableSetOf() } += satellite
-            adjacent.getOrPut(parent) { mutableSetOf() } += satellite
-            adjacent.getOrPut(satellite) { mutableSetOf() } += parent
+        companion object {
+            private val pattern = """(\w+)\)(\w+)""".toRegex()
         }
-        this.orbitedBy = orbitedBy
-        this.adjacent = adjacent
-    }
 
-    fun totalNumberOfOrbits(): Int = orbitedBy.keys.sumBy { numberOfOrbitsOf(it) }
+        val orbitedBy: Map<String, Set<String>>
+        val adjacent: Map<String, Set<String>>
 
-    fun numberOfOrbitsOf(name: String): Int = orbitedBy[name]?.sumBy { 1 + numberOfOrbitsOf(it) } ?: 0
+        init {
+            val orbitedBy = mutableMapOf<String, MutableSet<String>>()
+            val adjacent = mutableMapOf<String, MutableSet<String>>()
+            input.forEach {
+                val (parent, satellite) = pattern.find(it)!!.destructured
+                orbitedBy.getOrPut(parent) { mutableSetOf() } += satellite
+                adjacent.getOrPut(parent) { mutableSetOf() } += satellite
+                adjacent.getOrPut(satellite) { mutableSetOf() } += parent
+            }
+            this.orbitedBy = orbitedBy
+            this.adjacent = adjacent
+        }
 
-    fun numberOfOrbitalTransfers(src: String, dst: String): Int = breadthFirstSearch(src, dst, mutableMapOf(), mutableListOf(src), mutableListOf()).minBy { it.size }!!.size - 3
+        fun totalNumberOfOrbits(): Int = orbitedBy.keys.sumBy { numberOfOrbitsOf(it) }
 
-    fun breadthFirstSearch(
-        src: String,
-        dst: String,
-        visited: MutableMap<String, Boolean>,
-        path: MutableList<String>,
-        results: MutableList<List<String>>
-    ): List<List<String>> {
-        if (src == dst) {
-            results.add(path.toList())
+        fun numberOfOrbitsOf(name: String): Int = orbitedBy[name]?.sumBy { 1 + numberOfOrbitsOf(it) } ?: 0
+
+        fun numberOfOrbitalTransfers(src: String, dst: String): Int = breadthFirstSearch(src, dst, mutableMapOf(), mutableListOf(src), mutableListOf()).minBy { it.size }!!.size - 3
+
+        fun breadthFirstSearch(
+            src: String,
+            dst: String,
+            visited: MutableMap<String, Boolean>,
+            path: MutableList<String>,
+            results: MutableList<List<String>>
+        ): List<List<String>> {
+            if (src == dst) {
+                results.add(path.toList())
+                return results
+            }
+            visited[src] = true
+            adjacent[src]?.forEach {
+                if (visited[it] == true) return@forEach
+                path.add(it)
+                breadthFirstSearch(it, dst, visited, path, results)
+                path.remove(it)
+            }
+            visited[src] = false
             return results
         }
-        visited[src] = true
-        adjacent[src]?.forEach {
-            if (visited[it] == true) return@forEach
-            path.add(it)
-            breadthFirstSearch(it, dst, visited, path, results)
-            path.remove(it)
-        }
-        visited[src] = false
-        return results
+
     }
 
 }
-
