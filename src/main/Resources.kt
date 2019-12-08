14162d51da758a5c6fa84cf523bd47a7c287f4ff
Simon Marquis <contact@simon-marquis.fr>
2019-12-08 13:38:46 +0100

    Simplify Resources extension function

--- src/main/Resources.kt
+++ src/main/Resources.kt
@@ -3,16 +3,14 @@
  */
 
 import java.io.File
+import java.net.URI
 
 internal object Resources {
 
-    fun resourceAsString(fileName: String, delimiter: String = ""): String =
-        File(Resources.javaClass.classLoader.getResource(fileName)!!.toURI())
-            .readLines()
-            .reduce { a, b -> "$a$delimiter$b" }
+    fun resourceAsString(fileName: String, delimiter: String = ""): String = resourceAsList(fileName).reduce { a, b -> "$a$delimiter$b" }
 
-    fun resourceAsList(fileName: String): List<String> =
-        File(Resources.javaClass.classLoader.getResource(fileName)!!.toURI())
-            .readLines()
+    fun resourceAsList(fileName: String): List<String> = File(fileName.toURI()).readLines()
+
+    private fun String.toURI(): URI = Resources.javaClass.classLoader.getResource(this)?.toURI() ?: throw IllegalArgumentException("Cannot find: $this")
 
 }
