d420061c6c10e7faf59c08ac0e9c05e0e21313a1
Simon Marquis <contact@simon-marquis.fr>
2019-12-08 13:43:34 +0100

    Simplify permutations extension function

--- src/main/Extensions.kt
+++ src/main/Extensions.kt
@@ -2,20 +2,13 @@
  * Copyright (c) 2019 Simon Marquis
  */
 
-fun <T> List<T>.permutations(): Set<List<T>> = when {
-    isEmpty() -> setOf()
-    size == 1 -> setOf(listOf(get(0)))
-    else -> {
-        val element = get(0)
-        drop(1).permutations()
-            .flatMap { sublist -> (0..sublist.size).map { i -> sublist.plusAt(i, element) } }
-            .toSet()
-    }
-}
-
-internal fun <T> List<T>.plusAt(index: Int, element: T): List<T> = when (index) {
-    !in 0..size -> throw Error("Cannot put at index $index because size is $size")
-    0 -> listOf(element) + this
-    size -> this + element
-    else -> dropLast(size - index) + element + drop(index)
-}
+fun <T> List<T>.permutations(): Set<List<T>> =
+    if (this.size <= 1) listOf(this)
+    else {
+        val elementToInsert = first()
+        drop(1).permutations().flatMap { permutation ->
+            (0..permutation.size).map { e ->
+                permutation.toMutableList().apply { add(e, elementToInsert) }
+            }
+        }
+    }.toSet()
