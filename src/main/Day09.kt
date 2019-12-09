5530e93eade1c45ca5723c5bb9580257b97799ca
Simon Marquis <contact@simon-marquis.fr>
2019-12-09 22:51:48 +0100

    Day 9

---
+++ src/main/Day09.kt
@@ -0,0 +1,76 @@
+import Day09.IntcodeComputer.Mode.*
+
+/*
+ * Copyright (c) 2019 Simon Marquis
+ */
+
+class Day09(input: String) {
+
+    private val program: List<Long> = input.split(",").map { it.toLong() }
+
+    fun part1(input: Int) = IntcodeComputer(program).diagnostic(input.toLong())
+
+    fun part2(input: Int) = part1(input)
+
+    private class IntcodeComputer(val rom: List<Long>) {
+
+        private sealed class Mode {
+            object Position : Mode()
+            object Immediate : Mode()
+            object Relative : Mode()
+        }
+
+        fun diagnostic(input: Long): MutableList<Long> {
+            val signals = mutableListOf<Long>()
+            val ram = LongArray(10000) {
+                rom.getOrElse(it) { 0L }
+            }
+            var index = 0
+            var base = 0
+
+            parsing@ while (index < ram.size) {
+                val instruction = ram[index]
+
+                fun mode(offset: Int): Mode = when (when (offset) {
+                    1 -> (instruction / 100)
+                    2 -> (instruction / 1000)
+                    3 -> (instruction / 10000)
+                    else -> throw UnsupportedOperationException()
+                } % 10) {
+                    0L -> Position
+                    1L -> Immediate
+                    2L -> Relative
+                    else -> throw UnsupportedOperationException()
+                }
+
+                fun read(offset: Int) = when (mode(offset)) {
+                    Position -> ram[ram[index + offset].toInt()]
+                    Immediate -> ram[index + offset]
+                    Relative -> ram[ram[index + offset].toInt() + base]
+                }
+
+                fun write(offset: Int, value: Long) = when (mode(offset)) {
+                    Position -> ram[ram[index + offset].toInt()] = value
+                    Immediate -> throw UnsupportedOperationException()
+                    Relative -> ram[ram[index + offset].toInt() + base] = value
+                }
+
+                when (val opcode = (instruction % 100).toInt()) {
+                    1 -> write(3, read(1) + read(2)).also { index += 4 }
+                    2 -> write(3, read(1) * read(2)).also { index += 4 }
+                    3 -> write(1, input).also { index += 2 }
+                    4 -> signals.add(read(1)).also { index += 2 }
+                    5 -> if (read(1) != 0L) index = read(2).toInt() else index += 3
+                    6 -> if (read(1) == 0L) index = read(2).toInt() else index += 3
+                    7 -> write(3, if (read(1) < read(2)) 1 else 0).also { index += 4 }
+                    8 -> write(3, if (read(1) == read(2)) 1 else 0).also { index += 4 }
+                    9 -> base += read(1).also { index += 2 }.toInt()
+                    99 -> return (signals)
+                    else -> throw UnsupportedOperationException(opcode.toString())
+                }
+            }
+            return signals
+        }
+    }
+
+}
---
+++ src/test/Day09Test.kt
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) 2019 Simon Marquis
+ */
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+
+@DisplayName("Day 09")
+internal class Day09Test {
+
+    // Given
+    val actualInput = Resources.resourceAsString("Day09.txt")
+
+    @Nested
+    @DisplayName("Part 1")
+    inner class Part1 {
+
+        @Test
+        fun `Matches example 1`() {
+            // When
+            val input = "109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99"
+            val day = Day09(input)
+            // Then
+            assertThat(day.part1(11111)).isEqualTo(input.split(",").map { it.toLong() })
+        }
+
+        @Test
+        fun `Matches example 2`() {
+            // When
+            val day = Day09("1102,34915192,34915192,7,4,7,99,0")
+            // Then
+            assertThat(day.part1(1).last()).isEqualTo(1219070632396864)
+        }
+
+        @Test
+        fun `Matches example 3`() {
+            // When
+            val day = Day09("104,1125899906842624,99")
+            // Then
+            assertThat(day.part1(1).last()).isEqualTo(1125899906842624)
+        }
+
+        @Test
+        fun `Actual answer`() {
+            // When
+            val day = Day09(actualInput)
+            // Then
+            assertThat(day.part1(1).last()).isEqualTo(3241900951)
+        }
+    }
+
+    @Nested
+    @DisplayName("Part 2")
+    inner class Part2 {
+
+        @Test
+        fun `Actual answer`() {
+            // When
+            val day = Day09(actualInput)
+            // Then
+            assertThat(day.part2(2).last()).isEqualTo(83089)
+        }
+    }
+
+}
